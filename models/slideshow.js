3011a755e36465262040a20b73b75fb8a99048a0
Richard Huang <flyerhzm@gmail.com>
2012-02-28 11:51:10 -0800

    remove debug info

--- models/slideshow.js
+++ models/slideshow.js
@@ -13,12 +13,10 @@ var Slideshow = new Schema({
 });
 
 Slideshow.virtual('user_link').get(function() {
-  console.log(this.url.replace(/\/[^/]+$/, ""));
   return this.url.replace(/\/[^/]+$/, "");
 });
 
 Slideshow.virtual('embed').get(function() {
-  console.log('<div style="width:425px" id="__ss_' + this.slideshare_id + '"> <strong style="display:block;margin:12px 0 4px"><a href="' + this.url + '" title="' + this.title + '" target="_blank">' + this.title + '</a></strong> <iframe src="http://www.slideshare.net/slideshow/embed_code/' + this.slideshare_id + '?rel=0" width="425" height="355" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe> <div style="padding:5px 0 12px"> View more presentations from <a href="' + this.user_link + '" target="_blank">' + this.username + '</a> </div> </div>');
   return '<div style="width:425px" id="__ss_' + this.slideshare_id + '"> <strong style="display:block;margin:12px 0 4px"><a href="' + this.url + '" title="' + this.title + '" target="_blank">' + this.title + '</a></strong> <iframe src="http://www.slideshare.net/slideshow/embed_code/' + this.slideshare_id + '?rel=0" width="425" height="355" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe> <div style="padding:5px 0 12px"> View more presentations from <a href="' + this.user_link + '" target="_blank">' + this.username + '</a> </div> </div>';
 });
 
