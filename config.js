586bbe884ee87d3801428c9647f64b6a6637a648
Richard Huang <flyerhzm@gmail.com>
2012-02-27 16:45:45 -0800

    do not ignore config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+var config = {
+    db: "mongodb://127.0.0.1/nodeslide"
+  , slideshare_key: ""
+  , slideshare_secret: ""
+  , crawl_size: 5
+}
+
+if (process.env.MONGOLAB_URL) {
+  config["db"] = process.env.MONGOLAB_URI;
+}
+
+if (process.env.SLIDESHARE_KEY) {
+  config["slideshare_key"] = process.env.SLIDESHARE_KEY;
+}
+if (process.env.SLIDESHARE_SECRET) {
+  config["slideshare_secret"] = process.env.slideshare_secret;
+}
+
+exports.config = config;
