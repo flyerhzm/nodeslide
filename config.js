c95f6ee2c6e44ee518f6896146f1a2a508115af3
Richard Huang <flyerhzm@gmail.com>
2012-03-01 15:48:43 -0800

    fix typo

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var processEnv = process.env;
 var config = {
   db: processEnv.MONGOLAB_URI ? processEnv.MONGOLAB_URI : "mongodb://127.0.0.1/nodeslide",
   slideshare_key: processEnv.SLIDESHARE_KEY ? processEnv.SLIDESHARE_KEY : "",
-  slideshare_secret: processEnv.SLIDESHARE_SECRET ? porcessEnv.SLIDESHARE_SECRET : "",
+  slideshare_secret: processEnv.SLIDESHARE_SECRET ? processEnv.SLIDESHARE_SECRET : "",
   crawl_size: processEnv.CRAWL_SIZE ? processEnv.CRAWL_SIZE : 5
 };
 
